74cf2f78c7abe30fcc232dc240a6bb59e6946fd4
AndriusKovalevskij <andrius.kovalevskij@gmail.com>
2025-11-27 15:21:28 +0200

    Add files via upload

--- student.h
+++ student.h
@@ -7,14 +7,14 @@
 #include <algorithm>
 #include <numeric>
 
-class Studentas {
+// Studentas - isvestine klase is Zmogus
+class Studentas : public Zmogus {
 private:
-    std::string vardas_;
-    std::string pavarde_;
     std::vector<int> ndpazymiai_;
     int egzrezultatas_;
     double galutinis_vidurkis_;
     double galutine_mediana_;
+
 
 public:
     // Konstruktoriai
@@ -23,23 +23,19 @@ public:
     Studentas(const std::string& vardas, const std::string& pavarde);  // Konstruktorius su parametrais
     Studentas(const Studentas& other);  // Copy konstruktorius
 
-    // Destruktorius
-    ~Studentas();
+    // Destruktorius (override bazines klases)
+    ~Studentas() override;
 
     // Operatoriai
     Studentas& operator=(const Studentas& other);  // Copy assignment
 
     // Get'eriai (inline, const)
-    inline std::string vardas() const { return vardas_; }
-    inline std::string pavarde() const { return pavarde_; }
     inline const std::vector<int>& ndPazymiai() const { return ndpazymiai_; }
     inline int egzRezultatas() const { return egzrezultatas_; }
     inline double galutinisVidurkis() const { return galutinis_vidurkis_; }
     inline double galutineMediana() const { return galutine_mediana_; }
 
     // Set'eriai
-    void setVardas(const std::string& vardas) { vardas_ = vardas; }
-    void setPavarde(const std::string& pavarde) { pavarde_ = pavarde; }
     void setEgzRezultatas(int egz) { egzrezultatas_ = egz; }
     void addNdPazymys(int pazymys) { ndpazymiai_.push_back(pazymys); }
     void setNdPazymiai(const std::vector<int>& pazymiai) { ndpazymiai_ = pazymiai; }
@@ -47,20 +43,26 @@ public:
     // Galutinio balo skaiciavimas
     double galBalas(double (*skaiciavimas)(const std::vector<int>&)) const;
 
-    // Skaitymas is srauto
-    std::istream& readStudent(std::istream& is);
+    // Implementacija virtualiu metodu is bazines klases
+    std::istream& skaityti(std::istream& is) override;
+    std::ostream& spausdinti(std::ostream& os) const override;
 
-    // Isvedimas i srauta
+    // Backward compatibility metodai (naudoja naujus override metodus)
+    std::istream& readStudent(std::istream& is);
     std::ostream& writeStudent(std::ostream& os) const;
 
-    // Metodas skaiciuoti galutinius balus (vidurki ir mediana)
+    // Override info metodas
+    void info() const override;
+
+    // Metodas skaiciuoti galutinius balus
     void skaiciuotiGalutini();
 
-
+    // Friend operatoriai
     friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
     friend std::istream& operator>>(std::istream& is, Studentas& s);
 };
 
+// Palyginimo funkcijos
 bool compare(const Studentas& a, const Studentas& b);
 bool comparePagalVarda(const Studentas& a, const Studentas& b);
 bool comparePagalPavarde(const Studentas& a, const Studentas& b);
