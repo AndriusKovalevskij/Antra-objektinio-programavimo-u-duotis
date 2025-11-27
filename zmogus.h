578a700b8f7147fa5276452b5710c4b30d63a404
AndriusKovalevskij <andrius.kovalevskij@gmail.com>
2025-11-27 14:40:42 +0200

    Add files via upload

--- zmogus.h
+++ zmogus.h
@@ -10,5 +10,13 @@ protected:
     std::string vardas_;
     std::string pavarde_;
 
+    public:
+    // Konstruktoriai
+    Zmogus();  // Default konstruktorius
+    Zmogus(const std::string& vardas, const std::string& pavarde);  // Konstruktorius su parametrais
+    Zmogus(const Zmogus& other);  // Copy konstruktorius
+
+    // Virtualus destruktorius
+    virtual ~Zmogus();
 
 #endif // ZMOGUS_H_INCLUDED
