0cf4fa4e1e3b8e8a42c5c6b0fa1d9ce5b4fc9159
AndriusKovalevskij <andrius.kovalevskij@gmail.com>
2025-11-27 14:46:08 +0200

    Add files via upload

--- zmogus.cpp
+++ zmogus.cpp
@@ -1,1 +1,21 @@
 #include "zmogus.h"
+
+// Default konstruktorius
+Zmogus::Zmogus() : vardas_(""), pavarde_("") {
+}
+
+// Konstruktorius su parametrais
+Zmogus::Zmogus(const std::string& vardas, const std::string& pavarde)
+    : vardas_(vardas), pavarde_(pavarde) {
+}
+
+// Copy konstruktorius
+Zmogus::Zmogus(const Zmogus& other)
+    : vardas_(other.vardas_), pavarde_(other.pavarde_) {
+}
+
+// Virtualus destruktorius
+Zmogus::~Zmogus() {
+    vardas_.clear();
+    pavarde_.clear();
+}
